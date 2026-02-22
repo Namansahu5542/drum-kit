9744f9611d94ab4d23ddfdda042b43e6eb548677
Naman Sahu <namansahu5542@gmail.com>
2026-02-22 14:36:27 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function drum() {
+    document.addEventListener("keydown", (e) => {
+        let ele = document.body.querySelector(`.box[data-key="${e.keyCode}"]`);
+        let a1 = document.body.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+        if (a1 && ele) {
+            ele.classList.add("effect");
+            setInterval(() => {
+                ele.classList.remove("effect");
+            }, 250);
+            a1.currentTime = 0;
+            a1.play();
+
+        }
+
+
+    });
+
+}
+drum();
